568fec75b9fe2bb6183c0a8945fc2870870beb01
AdegoLabs <alphagoblin4@gmail.com>
2019-08-12 23:16:21 +0300

    Add files via upload

---
+++ new_template_utf_8_selected.php
@@ -0,0 +1,12 @@
+<?php
+
+$xhe_host = "127.0.0.1:7022";
+
+// The following code is required to properly run XWeb Human Emulator
+require("[[TEMPLATE]]/xweb_human_emulator.php");
+
+[[SELECTED]]
+
+// Quit
+$app->quit();
+?>
